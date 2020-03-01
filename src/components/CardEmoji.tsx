7278c0be2f95a5cc19b62ba239aac55743975c22
kyoncy <kyota.n.2014@gmail.com>
2020-03-01 22:56:11 +0900

    :sparkles: add react-toastify

--- src/components/CardEmoji.tsx
+++ src/components/CardEmoji.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { Card, Popup, Grid, Button, Header } from 'semantic-ui-react';
 import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+import { css } from 'glamor'
+import 'react-toastify/dist/ReactToastify.css';
 import EmojiArea from './EmojiArea';
 
 interface CardEmojiProps {
@@ -10,12 +13,33 @@ interface CardEmojiProps {
   name?: string;
 };
 
+toast.configure({
+  position: "bottom-left",
+  autoClose: 2000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: false,
+});
+
 const CardEmoji = ({
   emoji = '❌',
   backgroundColor = '#ffffff',
   name = 'none',
 }: CardEmojiProps) => {
   const [t] = useTranslation();
+  const notify = (text: string, backgroundColor: string) => toast(`Copy ${text}`, {
+    className: css({
+      fontWeight: 'bold',
+      backgroundColor: `${backgroundColor} !important`,
+      color: 'rgba(0,0,0,.87) !important'
+    }),
+    bodyClassName: css({
+      fontSize: '18px',
+      fontFamily: 'Apple Color Emoji,Segoe UI Emoji,Noto Color Emoji,Segoe UI Symbol,Android Emoji,EmojiSymbols',
+    })
+  });
+
   return (
     <Popup
       content={<p style={{ fontWeight: 'bold', userSelect: 'none' }}>Copy text :{name}:</p>}
@@ -41,13 +65,13 @@ const CardEmoji = ({
             fontFamily: 'Apple Color Emoji,Segoe UI Emoji,Noto Color Emoji,Segoe UI Symbol,Android Emoji,EmojiSymbols',
           }}>{emoji}</Header>
           <CopyToClipboard text={emoji}>
-            <Button>Direct Copy</Button>
+            <Button onClick={() => notify(emoji, backgroundColor)}>Direct Copy</Button>
           </CopyToClipboard>
         </Grid.Column>
         <Grid.Column textAlign='center'>
           <Header as='h4' style={{ userSelect: 'none' }}>{`:${name}:`}</Header>
           <CopyToClipboard text={`:${name}:`}>
-            <Button>Alias Copy</Button>
+            <Button onClick={() => notify(`:${name}:`, backgroundColor)}>Alias Copy</Button>
           </CopyToClipboard>
         </Grid.Column>
       </Grid>
