19e6b41a30a325da19009a59e2d7f558aa5fadc0
kyoncy <kyota.n.2014@gmail.com>
2020-03-01 14:30:13 +0900

    :sparkles: add top visual

---
+++ src/components/TopVisual.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react'
+
+const TopVisual = () => (
+  <div style={{ padding: '20px' }}>
+    <Header as='h1' style={{
+      textAlign: 'center',
+      fontSize: '70px',
+    }}>
+      Emojinja
+    </Header>
+  </div>
+);
+
+export default TopVisual;
